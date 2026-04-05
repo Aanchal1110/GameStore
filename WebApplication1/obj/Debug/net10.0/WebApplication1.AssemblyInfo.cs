a5299fc930837b225562a0dfee29ae47c16da48a
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-05 13:28:02 +0530

    To do database migration added 'Microsoft.EntityFrameWorkCore.Design' Package

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e12cf5c410d0ef802d74a8feeefdfdb5d920b9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c461f0db2ea0bedb54b2a6488301fda5b3c904d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

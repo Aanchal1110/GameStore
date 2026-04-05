bc35583cd490b7de480de8f18c60997cded719e0
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-05 15:37:44 +0530

    Added a new function to the extension class

--- WebApplication1/Data/DataExtension.cs
+++ WebApplication1/Data/DataExtension.cs
@@ -1,6 +1,7 @@
 //The class that contains the logic for automatic migration of any new change in the app to the database
 
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1.Data;
 
@@ -12,6 +13,26 @@ public static class DataExtension
         using var scope=app.Services.CreateScope();
         var dbContext=scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+    public static void AddGameStoreDB(this WebApplicationBuilder builder)
+    {
+        var conString="Data Source=GameStore.db";
+        builder.Services.AddSqlite<GameStoreContext>(conString,
+optionsAction:options =>options.UseSeeding((context, _) =>
+{
+    if (!context.Set<Genre>().Any())
+    {
+        context.Set<Genre>().AddRange(
+            new Genre{name="Fighting"},
+            new Genre{name="RPG"},
+            new Genre{name="Platformer"},
+            new Genre{name="Sports"}
+        );
+        context.SaveChanges();
+    }
+})
+
+);
     }
 
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,15 +2,12 @@ using WebApplication1.Data;
 using WebApplication1.Dtos;
 using WebApplication1.Endpoints;
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
+builder.AddGameStoreDB();
 
-
-
-
-var conString="Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(conString);
 var app = builder.Build();
 app.MapGamesEndPoints();
 app.MigrateDb();

b5d6152c177e46395027fe7a796cb4e9a6f8a366
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-05 14:29:08 +0530

    Changes made to remove unncessary sqlite commands from terminal

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a45d76e3aaf1817866ed3c611a3100d177795aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ea857a2614bb633708b2806d7f9d450db3c6e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
